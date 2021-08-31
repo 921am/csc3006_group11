a889a8b152a4793d96dc88ff5e3cac25f844d297
921am <madgaleneyip25@gmail.com>
2021-08-31 03:08:16 +0000

    clean up and streamline code

--- Software/csc3006_group11GrovePi.py
+++ Software/csc3006_group11GrovePi.py
@@ -8,55 +8,54 @@ import math
 light_sensor = 0 # Connect the Grove Light Sensor to analog port A0
 
 # SIG,NC,VCC,GND
-buzzer = 3
 dht_sensor = 2 # The Sensor goes on digital port 2.
 dhtVal = 0 # The Blue colored sensor.
 
 # Turn on once sensor exceeds threshold resistance
-threshold = 200
+threshold = 20
 
 grovepi.pinMode(light_sensor,"INPUT")
 # grovepi.pinMode(led,"OUTPUT")
 
 while True:
-	try:
-            #Phototransistor
+    try:
+            # Phototransistor
             # Get sensor value
             lightVal = grovepi.analogRead(light_sensor)
             
-            # Calculate resistance of sensor in K
+            # Calculate resistance of sensor
             x = (float)(1023 - lightVal)
             if (x <= 0):
                 resistance = 0
             else:
                 resistance = (float)(1023 - lightVal) * 10 / lightVal
             
-            if resistance > threshold:
+            if resistance < threshold:
                 print("STERILISED")
                 # Send HIGH to switch on LED
             else:
                 # Send LOW to switch off LED
                 print("NOT STERILISED")
             print("lightVal = %d resistance = %0.2f" %(lightVal,  resistance))
-            time.sleep(.5)
+            time.sleep(2)
 
-	except IOError:
-		#activate buzzer
-		grovepi.digitalWrite(buzzer,1)
-		print("Phototransistor Error!")
-		time.sleep(1)
-		grovepi.digitalWrite(buzzer,0)
-		time.sleep(5)
+    except IOError:
+        # send error message to arduino
+        print("Phototransistor Error!")
+        time.sleep(.5)
 
-	try:
-		#DHT11
-		#The first parameter is the port, the second parameter is the type of sensor
-	        [temp,humidity] = grovepi.dht(dht_sensor,dhtVal)
-	        if math.isnan(temp) == False and math.isnan(humidity) == False:print("temp = %0.2f c humidity = %0.2f%%"%(temp,humidity))
-	except IOError:
-		#activate buzzer
-		grovepi.digitalWrite(buzzer,1)
-		print("DHT11 Error!")
-		time.sleep(1)
-		grovepi.digitalWrite(buzzer,0)
-		time.sleep(5)
+    try:
+        # DHT11
+        # The first parameter is the port, the second parameter is the type of sensor
+            [temp,humidity] = grovepi.dht(dht_sensor,dhtVal)
+            humidity = 73
+            if humidity < 80:
+                print ("DRY")
+            else:
+                print ("NOT DRY")
+            
+            if math.isnan(temp) == False and math.isnan(humidity) == False:print("temp = %0.2f c humidity = %0.2f%%"%(temp,humidity))
+            time.sleep(2)
+    except IOError:
+        # send error message to arduino
+        print("DHT11 Error!")                                                                                                  
