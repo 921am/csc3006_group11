5b12806d61369e6306df9b7fca10f4453fc93431
Magdalene Yip <56404672+921am@users.noreply.github.com>
2021-09-03 02:14:58 +0800

    added USB serial code

--- Software/csc3006_group11GrovePi.py
+++ Software/csc3006_group11GrovePi.py
@@ -3,6 +3,7 @@
 import time
 import grovepi
 import math
+import serial
 
 # SIG,NC,VCC,GND
 light_sensor = 0 # Connect the Grove Light Sensor to analog port A0
@@ -12,25 +13,27 @@ dht_sensor = 2 # The Sensor goes on digital port 2.
 dhtVal = 0 # The Blue colored sensor.
 
 # Turn on once sensor exceeds threshold resistance
-threshold = 20
+threshold = 300
 
 grovepi.pinMode(light_sensor,"INPUT")
 # grovepi.pinMode(led,"OUTPUT")
 
 while True:
+    ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+    ser.flush()
     try:
             # Phototransistor
             # Get sensor value
             lightVal = grovepi.analogRead(light_sensor)
             
-            # Calculate resistance of sensor
-            x = (float)(1023 - lightVal)
+            # Calculate uv dose
+            dose = lightVal * 30 #for the purpose of this project we will indicate the time as 30sec
             if (x <= 0):
                 resistance = 0
             else:
                 resistance = (float)(1023 - lightVal) * 10 / lightVal
             
-            if resistance < threshold:
+            if dose < threshold:
                 print("STERILISED")
                 # Send HIGH to switch on LED
             else:
@@ -42,8 +45,11 @@ while True:
     except IOError:
         # send error message to arduino
         print("Phototransistor Error!")
-        time.sleep(.5)
-
+        
+        ser.write(b"Phototransistor Error\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(20)
     try:
         # DHT11
         # The first parameter is the port, the second parameter is the type of sensor
@@ -58,4 +64,8 @@ while True:
             time.sleep(2)
     except IOError:
         # send error message to arduino
-        print("DHT11 Error!")                                                                                                  
+        print("DHT11 Error!")
+        ser.write(b"DHT11 Error\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(20)                                                                                                  
